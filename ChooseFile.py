ed791f18f654044dfbc395d23b2517742eac636f
dhacode <dheeraj.alim@gmail.com]>
2017-06-02 14:36:19 +0530

    Incorrect file text updated

--- ChooseFile.py
+++ ChooseFile.py
@@ -5,7 +5,7 @@ def choose_image(self):
 
     options = QFileDialog.Options()
     options |= QFileDialog.DontUseNativeDialog
-    fileName, _ = QFileDialog.getOpenFileName(self,"Choose Fille", "","All Files (*);;JPEG Files (*.jpg);;PNG Files(*.png)", options=options)
+    fileName, _ = QFileDialog.getOpenFileName(self,"Choose File", "","All Files (*);;JPEG Files (*.jpg);;PNG Files(*.png)", options=options)
     if fileName:
         self.file_path = fileName
         self.file_location.setText(self.file_path)
