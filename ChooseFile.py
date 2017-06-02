97bed104b8b2e3bfbe3e6834e622f46c0dbf5991
dhacode <dheeraj.alim@gmail.com]>
2017-06-02 14:53:51 +0530

    Selection for TIF file in Choose file box

--- ChooseFile.py
+++ ChooseFile.py
@@ -5,7 +5,10 @@ def choose_image(self):
 
     options = QFileDialog.Options()
     options |= QFileDialog.DontUseNativeDialog
-    fileName, _ = QFileDialog.getOpenFileName(self,"Choose File", "","All Files (*);;JPEG Files (*.jpg);;PNG Files(*.png)", options=options)
+    fileName, _ = QFileDialog.getOpenFileName(self,
+                                              "Choose File", "",
+                                              "All Files (*);;JPEG Files (*.jpg);;PNG Files(*.png);; TIF Files(*.tif)",
+                                              options=options)
     if fileName:
         self.file_path = fileName
         self.file_location.setText(self.file_path)
